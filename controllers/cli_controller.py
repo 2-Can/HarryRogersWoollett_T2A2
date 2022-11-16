873c3da8aa397e6782c568e256fea12c7e4ddf51
harry <harry.rw96@gmail.com>
2022-11-17 00:40:26 +1100

    added more song seeds

--- controllers/cli_controller.py
+++ controllers/cli_controller.py
@@ -60,7 +60,88 @@ def seed_db():
             song_year = "2022",
             duration = "243",
             song_link = "https://www.youtube.com/watch?v=3AoOfJP3r40"
-        )
+        ),
+        Song(
+            song_name = "Come On Let's Go",
+            artist = "Tyler The Creator",
+            genre = "Hip-Hop",
+            album = "I Know Nigo!",
+            song_year = "2022",
+            duration = "200",
+            song_link = "https://www.youtube.com/watch?v=zuuPitOYNY4"
+        ),
+        Song(
+            song_name = "SAOKO",
+            artist = "Rosalia",
+            genre = "Reggaeton",
+            album = "Motomami",
+            song_year = "2022",
+            duration = "137",
+            song_link = "https://www.youtube.com/watch?v=ApNQX6fVWQ4"
+        ),
+        Song(
+            song_name = "The Smoke",
+            artist = "The Smile",
+            genre = "Alternative Rock",
+            album = "A Light For Attracting Attention",
+            song_year = "2022",
+            duration = "220",
+            song_link = "https://www.youtube.com/watch?v=theOkhAWs2s"
+        ),
+        Song(
+            song_name = "Zipcodes",
+            artist = "Joey Bada$$",
+            genre = "Hip-Hop",
+            album = "2000",
+            song_year = "2022",
+            duration = "265",
+            song_link = "https://www.youtube.com/watch?v=Fy3070QgHV0"
+        ),
+        Song(
+            song_name = "Ain't No Thief",
+            artist = "Viagra Boys",
+            genre = "Post-Punk",
+            album = "Cave World",
+            song_year = "2022",
+            duration = "239",
+            song_link = "https://www.youtube.com/watch?v=EAJLz6rbUSg"
+        ),
+        Song(
+            song_name = "Hot Girl",
+            artist = "Charli XCX",
+            genre = "Pop",
+            album = "Bodies Bodies Bodies",
+            song_year = "2022",
+            duration = "156",
+            song_link = "https://www.youtube.com/watch?v=vKIU3Q5oiMg"
+        ),
+        Song(
+            song_name = "Cutie",
+            artist = "Shanti Celeste",
+            genre = "House",
+            album = "HES041",
+            song_year = "2022",
+            duration = "386",
+            song_link = "https://www.youtube.com/watch?v=mYWNROkV9uc"
+        ),
+        Song(
+            song_name = "Pledge Drive",
+            artist = "Cheekface",
+            genre = "Alternative Rock",
+            album = "Too Much To Ask",
+            song_year = "2022",
+            duration = "164",
+            song_link = "https://www.youtube.com/watch?v=lOSXqWTvygY"
+        ),
+        Song(
+            song_name = "All You Do",
+            artist = "Magdalena Bay",
+            genre = "Pop",
+            album = "Mercurial World Deluxe",
+            song_year = "2022",
+            duration = "268",
+            song_link = "https://www.youtube.com/watch?v=wxaJ8aiURgU"
+        ),
     ]
 
     db.session.add_all(songs)

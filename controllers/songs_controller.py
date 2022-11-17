325e5176c4953e88ced2397eb69c7b190ec6df17
harry <harry.rw96@gmail.com>
2022-11-17 18:16:44 +1100

    song update route

--- controllers/songs_controller.py
+++ controllers/songs_controller.py
@@ -1,4 +1,4 @@
-from flask import Blueprint
+from flask import Blueprint, request
 from init import db
 from flask_jwt_extended import jwt_required, get_jwt_identity
 from models.song import Song, SongSchema
@@ -16,6 +16,7 @@ def get_all_songs():
 
 # Get one song
 @songs_bp.route('/<int:song_id>/')
+@jwt_required()
 def get_one_song(song_id):
     stmt = db.select(Song).filter_by(song_id=song_id)
     song = db.session.scalar(stmt)
@@ -25,10 +26,47 @@ def get_one_song(song_id):
         return {'error': f'Song not found with id {song_id}'}, 404
 
 
-
 # Create song
+@songs_bp.route('/create/', methods=['POST'])
+@jwt_required()
+def create_song():
+    # Create a new song entry
+    data = SongSchema().load(request.json)
+
+    song = Song(
+        song_name = data['song_name'],
+        artist = data['artist'],
+        genre = data['genre'],
+        album = data['album'],
+        song_year = data['song_year'],
+        duration = data['duration'],
+        song_link = data['song_link'],
+    )
+    db.session.add(song)
+    db.session.commit()
+
+    return SongSchema().dump(song), 201
 
 # Update song
+@songs_bp.route('/<int:song_id>/', methods=['PUT', 'PATCH'])
+@jwt_required()
+# Update a song's details
+def update_song(song_id):
+    stmt = db.select(Song).filter_by(song_id=song_id)
+    song = db.session.scalar(stmt)
+    if song:
+        song.song_name = request.json.get('song_name') or song.song_name
+        song.artist = request.json.get('artist') or song.artist
+        song.genre = request.json.get('genre') or song.genre
+        song.album = request.json.get('album') or song.album
+        song.song_year = request.json.get('song_year') or song.song_year
+        song.duration = request.json.get('duration') or song.duration
+        song.song_link = request.json.get('song_link') or song.song_link
+        db.session.commit()
+        return SongSchema().dump(song)
+    else:
+        return {'error': f'Song not found with id {song_id}'}, 404
+
 
 # Delete Song
 
